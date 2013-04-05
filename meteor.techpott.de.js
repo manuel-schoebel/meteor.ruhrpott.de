9819f670e0db9fcc12a8424bbb071b3cb4422293
Manuel Schoebel <mail@manuelschoebel.de>
2013-04-05 21:02:10 +0200

    basic meteor app

--- meteor.techpott.de.js
+++ meteor.techpott.de.js
@@ -1,6 +1,8 @@
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to myapp.";
+    return 'Hello Meteor Ruhrpott';
+    //var greeting = Session.get('greeting') || 'empty';
+    //return greeting;
   };
 
   Template.hello.events({
@@ -9,11 +11,16 @@ if (Meteor.isClient) {
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
     }
+    // ,
+    // 'mouseover input' : function () {
+    //   console.log("hover");
+    // }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    console.log('Meteor started');
   });
 }
