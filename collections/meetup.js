db21c9ea48efc921487128d7381bd8a963b2f601
Manuel Schoebel <mail@manuelschoebel.de>
2013-04-06 00:47:05 +0200

    security and users added

--- collections/meetup.js
+++ collections/meetup.js
@@ -1,1 +1,10 @@
 Meetup = new Meteor.Collection('meetup');
+
+Meetup.allow({
+  insert: function(userId, meetup){
+    return (userId) ? true : false;
+  },
+  remove: function(userId, meetup){
+    return (userId) ? true : false;
+  }
+});
--- collections/topic.js
+++ collections/topic.js
@@ -1,1 +1,13 @@
 Topic = new Meteor.Collection('topic');
+
+Topic.allow({
+  insert: function(userId, topic){
+    return (userId) ? true : false;
+  },
+  update: function(userId, meetup, fields, modifier){
+    return (userId) ? true: false;
+  },
+  remove: function(userId, meetup){
+    return (userId) ? true : false;
+  }
+});
